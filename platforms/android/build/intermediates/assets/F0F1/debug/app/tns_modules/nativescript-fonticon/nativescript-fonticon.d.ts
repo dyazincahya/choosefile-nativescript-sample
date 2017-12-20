4c6882ce8a3c3ebfcb04f1e3134bbf0543332571
dyazincahya <cahyadyazin@yahoo.com>
2017-12-20 09:37:19 +0700

    Fix the missing file

--- platforms/android/build/intermediates/assets/F0F1/debug/app/tns_modules/nativescript-fonticon/nativescript-fonticon.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TNSFontIcon {
-    static css: any;
-    static paths: any;
-    static debug: boolean;
-    static loadCss(): void;
-}
-export declare function fonticon(value: string): string;
